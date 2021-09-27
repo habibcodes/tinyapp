1b823613f15dbea54acf032d19b5d89c91def8b9
habibcodes <habib.codes@gmail.com>
2021-09-27 16:26:09 -0400

    added the urls_index file and link to page for shortening url

--- express_server.js
+++ express_server.js
@@ -18,8 +18,14 @@ app.get('/', (req, res) => {
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
+
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b> World</b></body></html>\n');
+});
+
+app.get('/urls', (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
 });
 
 
